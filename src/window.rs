30101db3d1665f04a7932efcf8d66bc53bb9fe8b
Vancha March <vancha@localhost.localdomain>
2021-03-22 22:36:04 +0100

    code cleanup

--- src/window.rs
+++ src/window.rs
@@ -8,11 +8,28 @@ impl Window {
     pub fn new() -> Self {
         let builder = gtk::Builder::new_from_resource("/org/example/App/window.ui");
 
+        //a css provider thing. to provide some css
         let provider = gtk::CssProvider::new();
-        provider.load_from_path(&"/home/vancha/style.css").unwrap();
 
-        let contextu = gtk::StyleContext::new();
+        //the css to use with this application. because gtk adds background images by default, i need to remove them here to set a bg color
+        let mahcss = "  .greenbutton { background-image: none; border-image: none;background-color:#00ff00;}
+                        .redbutton { background-image: none; border-image: none;background-color:#ff0000;}
+                        .yellowbutton { background-image: none; border-image: none;background-color:#ffff00;}
+                        .bluebutton { background-image: none; border-image: none;background-color:#0000ff;}
+
+                        .greenbutton:hover { background-color:#00AA00; }
+                        .redbutton:hover { background-color:#AA0000; }
+                        .yellowbutton:hover { background-color:#AAAA00; }
+                        .bluebutton:hover { background-color:#0000AA; }
+                        ";
+
+        //sets mahcss as the css styles, but converts them to <u8>
+        provider.load_from_data(mahcss.as_bytes());
+
+        //gets a screen, only required for the add_provider_for_screen call
         let screen = gdk::Screen::get_default().unwrap();
+
+        //the line that binds the actual css rules to this application
         gtk::StyleContext::add_provider_for_screen(&screen, &provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
 
         //this represents the entire view, it holds all other widgets (it's described in window.ui)
@@ -30,8 +47,6 @@ impl Window {
             .get_object("flowbox1")
             .expect("Failed to find the flow box object");
 
-        //let gree = gdk::RGBA{ alpha:1.0, blue:1.0, green:0.0, red:0.0, };
-
         //just a vec, that holds some buttons.
         let button_vec = vec![
             gtk::Button::new(),//If i can't override the default color chooser to not come up
@@ -40,35 +55,24 @@ impl Window {
             gtk::Button::new(),//if I can disable color chooser, this looks much cewler
         ];
 
+        //the styles that go with the buttons in the button_vec vec
+        let classes=vec!["greenbutton","redbutton","yellowbutton","bluebutton"];
 
-        let greenbutton = button_vec[0].clone();
-        let redbutton = button_vec[1].clone();
-        let yellowbutton = button_vec[2].clone();
-        let bluebutton = button_vec[3].clone();
-
-        let green_button_context = greenbutton.get_style_context();
-        green_button_context.add_class("greenbutton");
-
-        let red_button_context = redbutton.get_style_context();
-        red_button_context.add_class("redbutton");
-
-        let yellow_button_context = yellowbutton.get_style_context();
-        yellow_button_context.add_class("yellowbutton");
-
-        let blue_button_context = bluebutton.get_style_context();
-        blue_button_context.add_class("bluebutton");
-
-        //a timer that prints wub wub every second, vital to simons operation
+        /*a timer that prints wub wub every second, vital to simons operation
         let timer = gtk::timeout_add(1000, move || {
             //button_cloner.emit("activate", &[]);
             println!("wub wub");
             glib::Continue(true)
-        });
+        });*/
+
+
 
         //looping over the buttons in the vec to set their size, and also add them to the flowbox
-        for temp_but in button_vec {
+        for (i,temp_but) in button_vec.iter().enumerate() {
+            let button_context = temp_but.get_style_context();
+            button_context.add_class(classes[i]);
             temp_but.set_size_request(150, 150);
-            flowbox1.add(&temp_but);
+            flowbox1.add(temp_but);
         }
 
 
