aec6052171a6a5d4739751b70fa2c97cd18e3454
Vancha March <vancha@localhost.localdomain>
2021-03-22 21:23:42 +0100

    added pretty colors

--- src/window.rs
+++ src/window.rs
@@ -5,7 +5,6 @@ pub struct Window {
 }
 
 impl Window {
-
     pub fn new() -> Self {
         let builder = gtk::Builder::new_from_resource("/org/example/App/window.ui");
 
@@ -15,7 +14,7 @@ impl Window {
             .expect("Failed to find the window object");
 
         //the outerbox, a regular box layout, holds the colored buttons and the start button.
-        let outerbox:gtk::Box = builder
+        let outerbox: gtk::Box = builder
             .get_object("box1")
             .expect("Failed to find the flow box object");
 
@@ -24,14 +23,30 @@ impl Window {
             .get_object("flowbox1")
             .expect("Failed to find the flow box object");
 
-        //just a vec, that holds some buttons.
-        let button_vec = vec![gtk::Button::new(),gtk::Button::new(),gtk::Button::new(),gtk::Button::new()];
+        //let gree = gdk::RGBA{ alpha:1.0, blue:1.0, green:0.0, red:0.0, };
 
+        //just a vec, that holds some buttons.
+        let button_vec = vec![
+            gtk::ColorButton::new_with_rgba(&gdk::RGBA{ alpha:1.0, blue:1.0, green:0.0, red:0.0, }),
+            gtk::ColorButton::new_with_rgba(&gdk::RGBA{ alpha:1.0, blue:0.0, green:1.0, red:0.0, }),
+            gtk::ColorButton::new_with_rgba(&gdk::RGBA{ alpha:1.0, blue:0.0, green:0.0, red:1.0, }),
+            gtk::ColorButton::new_with_rgba(&gdk::RGBA{ alpha:1.0, blue:0.0, green:238.0,red:238.0,}),
+        ];
+
+        let button_cloner = button_vec[0].clone();
+
+        let button_cloner_special = button_vec[0].clone();
+
+        button_cloner_special.connect_clicked(|_| println!("weee"));
+
+
+       // button_cloner_special.set_property("color",&"blue");
         //a timer that prints wub wub every second, vital to simons operation
-        let timer = gtk::timeout_add(1000, ||{
-                                                println!("wub wub");
-                                                glib::Continue(true)
-                                                });
+        let timer = gtk::timeout_add(1000, move || {
+            //button_cloner.emit("activate", &[]);
+            println!("wub wub");
+            glib::Continue(true)
+        });
 
         //looping over the buttons in the vec to set their size, and also add them to the flowbox
         for temp_but in button_vec {
@@ -39,13 +54,11 @@ impl Window {
             flowbox1.add(&temp_but);
         }
 
-        let start_button = gtk::Button::new_with_label("boop");//clicking this button will start the timer
+        let start_button = gtk::Button::new_with_label("boop"); //clicking this button will start the timer
 
-
-        outerbox.pack_start(&start_button, false,false,0);
+        outerbox.pack_start(&start_button, false, false, 0);
 
         start_button.connect_clicked(|_| println!("the game is starting"));
-
 
         widget.show_all();
 
